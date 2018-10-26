bb21cbf7eec3462ad2fe66806fc648e83e269021
206081 <43931644+206081@users.noreply.github.com>
2018-10-26 19:05:17 +0200

    Add files via upload

---
+++ MovingSprite/AnimateSprite.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovingSprite
+{
+    public class AnimateSprite
+    {
+        public Texture2D Texture;     // texture
+
+        private float totalElapsed;   // elapsed time
+
+        private int rows;             // number of rows
+        private int columns;          // number of columns
+        private int width;            // width of a graphic
+        private int height;           // height of a graphic
+        private float animationSpeed; // pictures per second
+
+        private int currentRow;       // current row
+        private int currentColumn;    // current culmn
+
+        public void LoadGraphic(
+          Texture2D texture,
+          int rows,
+          int columns,
+          int width,
+          int height,
+          int animationSpeed
+          )
+            {
+                this.Texture = texture;
+                this.rows = rows;
+                this.columns = columns;
+                this.width = width;
+                this.height = height;
+                this.animationSpeed = (float)1 / animationSpeed;
+
+                totalElapsed = 0;
+                currentRow = 0;
+                currentColumn = 0;
+            }
+
+        public void Update(float elapsed)
+        {
+            
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                currentRow = 2;
+                Move(elapsed);
+            }
+                
+            else if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                currentRow = 0;
+                Move(elapsed);
+            }
+                
+            else if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                currentRow = 3;
+                Move(elapsed);
+
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                currentRow = 1;
+                Move(elapsed);
+            }
+            else
+            {
+                currentRow = 0;
+                Move(0);
+            }
+               
+        }
+
+        private void Move(float elapsed)
+        {
+            totalElapsed += elapsed;
+            if (totalElapsed > animationSpeed)
+            {
+                totalElapsed -= animationSpeed;
+                currentColumn += 1;
+
+                if (currentColumn >= columns)
+                     currentColumn = 0;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 position, Color color)
+        {
+            spriteBatch.Draw(
+                Texture,
+                new Rectangle((int)position.X, (int)position.Y, width, height),
+                new Rectangle(
+                  currentColumn * width,
+                  currentRow * height,
+                  width, height),
+                color
+                );
+        }
+    }
+}
---
+++ MovingSprite/Game1.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MovingSprite
+{
+    
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        AnimateSprite runAnimate;
+        private Vector2 position = new Vector2(100,100);
+        private float speed = 3f;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void LoadContent()
+        {
+
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            runAnimate = new AnimateSprite();
+            runAnimate.LoadGraphic(Content.Load<Texture2D>("run"), 4, 6, 100, 100, 6);
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            runAnimate.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+                position.Y -= speed;
+            else if (Keyboard.GetState().IsKeyDown(Keys.S))
+                position.Y += speed;
+            else if (Keyboard.GetState().IsKeyDown(Keys.A))
+                position.X -= speed;
+            else if (Keyboard.GetState().IsKeyDown(Keys.D))
+                position.X += speed;
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            runAnimate.Draw(spriteBatch, position, Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
